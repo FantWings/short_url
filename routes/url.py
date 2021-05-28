ce69288085c228a19ca0b021be8561d07d67dcb4
FantWings <zhijun@furry.top>
2021-05-28 18:34:29 +0400

    拆分token验证到before request

--- routes/url.py
+++ routes/url.py
@@ -9,58 +9,50 @@ from lib.redis import Redis
 url = Blueprint('url', __name__)
 
 
-@url.before_app_request
-def get_userid():
+@url.before_request
+def getUserId():
+    global token
     global userid
-    userid = Redis.read(
-        'session_{}'.format(request.headers.get('token', default=0)))
+    token = request.headers.get('token', default=False)
+    if not token:
+        return make_response(response(msg="需要登录", code=1), 200)
+    userid = Redis.read('session_{}'.format(token))
 
 
 @url.route('/getLists', methods=['GET'])
 def get_urls():
-    if userid:
-        results = t_url.query.filter_by(owner_id=userid).all()
-        data = []
-        for result in results:
-            data.append(
-                {
-                    'url_id': result.id,
-                    'short_url': result.short_url,
-                    'original_url': result.original_url,
-                    'vaild_time': result.vaild_time
-                }
-            )
-        return make_response(response(data=data), 200)
-    else:
-        return make_response(response(msg="该操作需要登录", status=1), 200)
+    results = t_url.query.filter_by(owner_id=userid).all()
+    data = []
+    for result in results:
+        data.append({
+            'url_id': result.id,
+            'short_url': result.short_url,
+            'original_url': result.original_url,
+            'vaild_time': result.vaild_time
+        })
+    return make_response(response(data=data), 200)
 
 
 @url.route('/add', methods=['POST'])
 def add_url():
-    if userid:
-        client_data = request.get_json()
-        url = client_data.get('url')
-        if t_url.query.filter_by(original_url=url).first() is None:
-            new_url = t_url(
-                short_url=genToken(8),
-                original_url=url,
-                owner_id=userid,
-            )
-            db.session.add(new_url)
-            db.session.commit()
-            return make_response(response(msg="地址添加成功"), 200)
-        else:
-            return make_response(response(msg="地址已经存在", status=1), 200)
+    client_data = request.get_json()
+    url = client_data.get('url')
+    if t_url.query.filter_by(original_url=url).first() is None:
+        new_url = t_url(
+            short_url=genToken(8),
+            original_url=url,
+            owner_id=userid,
+        )
+        db.session.add(new_url)
+        db.session.commit()
+        return make_response(response(msg="地址添加成功"), 200)
     else:
-        return make_response(response(msg="该操作需要登录", status=1), 200)
+        return make_response(response(msg="地址已经存在", code=1), 200)
 
 
 @url.route('/delete', methods=['DELETE'])
 def del_url():
-    if userid:
-        results = t_url.query.get(request.args.get('urlId'))
-        results.status = -1
-        db.session.commit()
-        return make_response(response(msg="操作成功"), 200)
-    else:
-        return make_response(response(msg="该操作需要登录", code=1), 200)
+    results = t_url.query.get(request.args.get('urlId'))
+    results.status = -1
+    db.session.commit()
+    return make_response(response(msg="操作成功"), 200)
---
+++ routes/user.py
@@ -0,0 +1,33 @@
+from flask import Blueprint, make_response, request
+
+# from sql.model import db
+from sql.tables.t_user import t_user
+from lib.interface import response
+from lib.redis import Redis
+
+user = Blueprint('user', __name__)
+
+
+@user.before_request
+def getUserId():
+    global token
+    global userid
+    token = request.headers.get('token', default=False)
+    if not token:
+        return make_response(response(msg="需要登录", code=1), 200)
+    userid = Redis.read('session_{}'.format(token))
+
+
+@user.route('/userInfo', methods=['GET'])
+def userInfo():
+    results = t_user.query.get(userid)
+    data = {
+        'uuid': results.uuid,
+        'avatar': results.avatar,
+        'email': results.email,
+        'phone': results.phone,
+        'qq': results.qq,
+        'vip_vaild': results.vip_vaild_time,
+        'active': results.active
+    }
+    return make_response(response(data=data), 200)
